9fb32bda76a78b2ba5f4e1937f32f5d32847377a
Sam <samfagelman@hotmail.com>
2022-09-06 15:17:46 +0100

    ticket 5 - GET exercises

--- App.js
+++ App.js
@@ -7,4 +7,12 @@ app.get(["/exercises"], getExercises);
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
 
+///////////////////////////////////
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
 module.exports = app;
--- controllers/exercises.controller.js
+++ controllers/exercises.controller.js
@@ -1,7 +1,33 @@
-const selectExercises = require("../models/exercises.model")
+const selectExercises = require("../models/exercises.model");
 
 exports.getExercises = (req, res, next) => {
-    selectExercises().then((result)=>{
-        res.status(200).send(result);
+  const sortBy = req.query.sort_by;
+  const order = req.query.order;
+  const bodyPart = req.query.body_part;
+  const equipment = req.query.equipment;
+  const target = req.query.target;
+  const id = req.query.id;
+
+  const validQueries = [
+    "sort_by",
+    "order",
+    "filter",
+    "body_part",
+    "equipment",
+    "target",
+    "id",
+  ];
+  if (Object.keys(req.query).length != 0) {
+    for (query in req.query) {
+      if (!validQueries.includes(query)) {
+        res.status(400).send({ msg: "Bad Request" });
+      }
+    }
+  }
+
+  selectExercises(sortBy, order, bodyPart, equipment, target, id)
+    .then((result) => {
+      res.status(200).send(result);
     })
-}
+    .catch(next);
+};
